5c4c05f75d41ea6f64f93b6195aa13cf662dbdf4
Jay <jaylimcode@gmail.com>
2021-05-20 22:25:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+android hoseo university intranet webview
+
+<img width="366" alt="KakaoTalk_20210519_163636519" src="https://user-images.githubusercontent.com/59774709/118986101-01315d00-b9ba-11eb-8337-5bc586343e83.png">
---
+++ app/src/main/java/com/hoseo/hoseoportal/MainActivity.java
@@ -0,0 +1,31 @@
+package com.hoseo.hoseoportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://intranet.hsu.ac.kr/");
+    }
+}
